de823766e3280d18cc9514dc8e49eb2515b7e076
subramanian-s-043 <113493600+subramanian-s-043@users.noreply.github.com>
2023-10-17 00:24:58 +0530

    Basics

---
+++ Encapsulation/Main.java
@@ -0,0 +1,25 @@
+package Encapsulation;
+class Employee {
+    private String name;
+    private int Salary;
+    //Getters-And-Setters
+    public String getName(){
+        return name;
+    }
+    public void setName(String Name){
+        this.name=Name;
+    }
+    public void setSalary(int s){
+        this.Salary=s;
+    }
+    public int getSalary(){
+        return Salary;
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        Employee e = new Employee();
+        e.setName("Deva"); //variable-name hidden.
+        System.out.println(e.getName());
+    }
+}
