04526755934f5fab96e82b8bd9f32877de656295
subramanian-s-043 <113493600+subramanian-s-043@users.noreply.github.com>
2023-10-22 00:22:28 +0530

    Inital

--- encapsulation/test.java
+++
@@ -1,9 +0,0 @@
-package encapsulation;
-public class test {
-    public static void main(String[] args){
-    long i =12345678;
-    int a = (int)i;
-    long b = a;
-    System.out.print(b);
-    }
-}
